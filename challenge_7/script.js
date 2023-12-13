6cc2d7ed28cc880f862d6fa740d6899d4df185e4
HidriSana <115165735+HidriSana@users.noreply.github.com>
2023-12-13 17:48:30 +0100

    challenge_7

---
+++ challenge_7/script.js
@@ -0,0 +1,17 @@
+function calcTip(note) {
+  const tipPercentage = note > 50 && note < 300 ? 0.15 : 0.2;
+  const tip = note * tipPercentage;
+  return tip;
+}
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+
+const tips = bills.map(calcTip);
+
+console.log(tips);
+
+const totals = bills.map((bill, index) => bill + tips[index]);
+
+console.log(totals);
+cd 
